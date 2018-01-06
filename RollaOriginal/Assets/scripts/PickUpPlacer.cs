ea9edacebc5261dee8269f734a434e54d8a85ab3
kandycoco <cocoawg@gmail.com>
2018-01-05 22:21:19 -0800

    randomized pickup spawn location

---
+++ RollaOriginal/Assets/scripts/PickUpPlacer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpPlacer : MonoBehaviour
+{
+    public Rotator Prefab;
+    public int NumberOfPickUps;
+    public int Length;
+    public int Width;
+
+    private Rotator[] PickUps;
+
+    private void Start()
+    {
+        PickUps = new Rotator[NumberOfPickUps];
+        for (int i = 0; i < NumberOfPickUps; i++)
+        {
+            float x = Random.Range(-Length, Length);
+            float y = .75f;
+            float z = Random.Range(-Width, Width);
+            Vector3 position = new Vector3(x, y, z);
+            PickUps[i] = Instantiate(Prefab, position, new Quaternion());
+        }
+    }
+}
