c142fbe71be1657fb9afa26a9c749f4859ec66d7
kandycoco <cocoawg@gmail.com>
2018-01-05 20:21:40 -0800

    init

---
+++ Roll a Ball - Poke/Assets/scripts/PlayerController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+    public float speed;
+    public Text countText;
+    public Text winText;
+    public Text winSubText;
+    public Text timerText;
+
+    private Rigidbody rb; //create variable to hold reference
+    private int count;
+    private float startTime;
+ 
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+        winSubText.text = "";
+        startTime = Time.time;
+    }
+
+    void Update()
+    {
+        float t = Time.time - startTime;
+
+        string minutes = ((int)t / 60).ToString("f2");
+        string seconds = (t % 60).ToString("f2");
+
+        timerText.text = minutes + ":" + seconds;
+
+    }
+
+
+    void FixedUpdate () {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+
+        rb.AddForce(movement * speed);
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+       if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        } 
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 8)
+        {
+            winText.text = "You Win!!";
+            winSubText.text = "Your Time was " + timerText.text;
+        }
+    
+    }
+
+    
+}
+
